f12fb1a53e793ba4a1d65810d11abc77cdb26006
Matthew Buller <mbuller@regis.edu>
2013-09-28 14:22:01 -0500

    business_2

---
+++ MSSE680/DAL/MSSE680.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSSE680
+{
+    class MSSE680
+    {
+        static void Main(String[] args)
+        { }
+    }
+}
